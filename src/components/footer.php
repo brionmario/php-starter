ae53f39cbd2a7a8846e6952c6b315eaa8928c9f3
Brion Mario <brion.2015283@iit.ac.lk>
2019-06-21 10:57:16 +0530

    chore(app): add composer demo

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,17 +1,33 @@
-<div data-role="footer" class="footer">
+<div class="footer">
   <ul class="footer-icons">
-    <li><a href="https://www.facebook.com/apareciumlabs/" target="_blank"><i class="fa fa-facebook-square"></i></a>
+    <li>
+      <a href="https://www.facebook.com/apareciumlabs/" target="_blank"
+        ><i class="fa fa-facebook"></i
+      ></a>
     </li>
-    <li><a href="https://twitter.com/apareciumlabs" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
-    <li><a href="https://github.com/apareciumlabs" target="_blank"><i class="fa fa-github-square"></i></a></li>
+    <li>
+      <a href="https://twitter.com/apareciumlabs" target="_blank"
+        ><i class="fa fa-twitter"></i
+      ></a>
+    </li>
+    <li>
+      <a href="https://github.com/apareciumlabs" target="_blank"
+        ><i class="fa fa-github"></i
+      ></a>
+    </li>
   </ul>
   <div class="copyright">
-    <p>&copy; Copyright
+    <p>
+      &copy; Copyright
       <script language="JavaScript" type="text/javascript">
         document.write(getCurrentYear());
       </script>
     </p>
-    <img src="assets/img/logo_aparecium_fox_only.svg" width="20px" height="20px" />
-    <p>Aparecium Labs. All rights reserved.</p>
+    <img
+      src="assets/img/logo_aparecium_fox_only.svg"
+      width="20px"
+      height="20px"
+    />
+    <p><a class="link" href="http://apareciumlabs.com" target="_blank">Aparecium Labs.</a> All rights reserved.</p>
   </div>
 </div>
--- src/composer-demo.php
+++ src/composer-demo.php
@@ -19,6 +19,7 @@
   <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
   <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
   <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+
   <!-- inject:css -->
   <!-- endinject -->
 
@@ -27,35 +28,34 @@
 </head>
 
 <body>
-<div data-role="page">
-  <?php require './components/sidebar.php'?><!-- /panel -->
   <?php require './components/header.php'?><!-- header -->
-  <div role="main" class="overlay ui-content main-content">
-    <div class="ui-grid-a ui-responsive">
-      <div class="composer-demo">
-        <div class="heading">
-          <img class="composer-logo" src="assets/img/composer.svg" height="100"/>
-          <h6>This seed project incorporates the power of composer. Please visit <a href="https://packagist.org/" target="_blank">https://packagist.org/</a> to browse packages and simply add them to to the project.</h6>
-          <p>Following is a demonstration of the already included <span class="pre">faker</span> library. Press the generate button to generate a random word.</p>
+  <div class="main-area">
+    <div class="container">
+      <div class="row">
+        <div class="composer-demo text-center mt-5">
+          <div class="heading">
+            <img class="composer-logo mb-4" src="assets/img/composer.svg" height="300"/>
+            <h6 class="text-dark">This seed project incorporates the power of composer. Please visit <a href="https://packagist.org/" target="_blank">https://packagist.org/</a> to browse packages and simply add them to to the project.</h6>
+            <p>Following is a demonstration of the already included <span class="pre">faker</span> library. Press the generate button to generate a random word.</p>
+          </div>
+          <div class="faker-demo-container">
+            <h4 id="fake-name" class="fake-name text-dark mt-4 mb-4">Fake Name</h4>
+            <button id="generate-btn" class="btn btn-primary btn-sm">Generate</button>
+          </div>
         </div>
-        <div class="faker-demo-container">
-          <h4 id="fake-name" class="fake-name">Fake Name</h4>
-          <button id="generate-btn" class="btn btn-primary btn-sm">Generate</button>
-        </div>
-      </div>
-    </div><!-- /grid-a -->
-  </div><!-- /content -->
+      </div><!-- /row -->
+    </div><!-- /container -->
+  </div><!-- /main-area -->
   <?php require './components/footer.php'?><!--footer -->
-</div><!-- page -->
-<script language="JavaScript" type="text/javascript">
-  $(document).ready(function() {
-    $("button").click(function() {
-      <?php
-      $faker = Faker\Factory::create();
-      echo 'document.getElementById("fake-name").innerHTML = "'. $faker->name .'";';
-      ?>
+  <script language="JavaScript" type="text/javascript">
+    $(document).ready(function() {
+      $("button").click(function() {
+        <?php
+        $faker = Faker\Factory::create();
+        echo 'document.getElementById("fake-name").innerHTML = "'. $faker->name .'";';
+        ?>
+      });
     });
-  });
-</script>
+  </script>
 </body>
 </html>
